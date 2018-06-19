7952cb9056c2ad2cfbbc0f468b8eea6f77970a7c
Angelica Fields <angelicafields08@gmail.com>
2018-06-19 10:42:37 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+   return document.querySelector('#nested .target')
+}
+
+function deepestChild(){
+  return document.querySelector('#grand-node div div div div')
+}
+
+function increaseRankBy(n) {
+ var lis = document.querySelectorAll('ul.ranked-list li')
+ for (let i = 0; i < lis.length; i++ ) {
+    lis[i].innerHTML = (1 + n + i).toString()
+ }
+}
